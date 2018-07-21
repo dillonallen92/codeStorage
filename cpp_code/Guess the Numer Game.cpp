ee9571d5a645321b5b8b6e3c2d6f4f3333382f0e
brenden748 <34994038+brenden748@users.noreply.github.com>
2018-07-21 13:03:30 -0700

    Update Guess the Numer Game.cpp

--- cpp_code/Guess the Numer Game.cpp
+++ cpp_code/Guess the Numer Game.cpp
@@ -1,12 +1,13 @@
 // Ch5 Comprehensive, High-Low Game.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#include <iostream>
-#include <Windows.h>
-#include <ctime>
-#include <cstdlib>
-#include <math.h>
+#include "stdafx.h" // for VS only
+#include <iostream> 
+#include <Windows.h> // for Sleep(milliseconds)
+#include <ctime> // for std::time(0) to get clock
+#include <cstdlib> // for rand() and srand()
+#include <math.h> // for abs() absolute value
+#include <stdlib.h>// for system("CLS") to clear the console
 
 
 
@@ -30,41 +31,106 @@ bool checkGuess(int number, int guess) {
 		return true; 
 	}
 	else if (guess > number) {
-		std::cout << "Your guess " << guess << " was too high \n";
+		std::cout << "Your guess " << guess << " was too high \n\n";
 		return false; 
 	}
 	else if (guess < number) {
-		std::cout << "Your guess " << guess << " was too low\n";
+		std::cout << "Your guess " << guess << " was too low\n\n";
 		return false;
 	 }
 }
+
+
+int getAndCheckInput() { // attempts to error handle for bad input during the game
+	std::cin.clear(); 
+	bool invalid = true;
+	
+
+	while (invalid) {
+		int x;
+		
+		std::cin >> x;
+		//std::cin.ignore(32767, '\n');
+		if(std::cin.fail()){ //returns true if cin fails to extract 
+			std::cout << "Input unrecognized, try again. \n";
+			std::cin.clear();  // resets input steam
+			std::cin.ignore(32767, '\n');   // ignore remaining characters and restart 
+			
+		}
+		else {
+			invalid = false; // this is to prevent the loop from running longer
+			return x;
+		}
+	}
+	
+}
+
+
+
+bool checkPlayAgain() { // checks for bad input at the end 
+	std::cout << "Would you like to play again? (y/n)\n";
+	
+	bool invalid = true;
+	while (invalid) {
+		char playAgain;
+		std::cin >> playAgain;
+		
+		if (std::cin.fail()) {  // its best to check if it failed first
+			std::cout << "Input unrecognized, try again. \n";
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			
+		}
+		else if (playAgain == 'y'){
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			invalid = false;
+			return true;
+			break;
+			
+		}
+		else if (playAgain == 'n') {
+			invalid = false;
+			return false; 
+			break;
+			
+		}
+		else {
+			std::cout << "Input unrecognized, try again. \n";
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+	}
+	
+}
+
+
 
 
 int main()
 {
 	bool exitGame = false;
 	
-
+	system("CLS"); // clears console
 	while (!exitGame) {
 		std::cout << "\n\nChoose minimum: \n";
-		int min;
-		std::cin >> min;
+		int min = getAndCheckInput();
+		
 		std::cout << "Choose maximum: \n";
-		int max;
-		std::cin >> max;
+		int max = getAndCheckInput();
 
 		// get the random number
 		int number = getRandomNumber(min, max);
 		
-		std::cout << "Guess a number between " << min << "-" << max << " \n";
+		std::cout << "Guess an integer between " << min << "-" << max << " \n";
 		bool guessing = true;
 		bool correct = false;
 		int guessCount = 0;
 
 		while (guessing && (guessCount !=5)) {
 			
-			int guess; 
-			std::cin >> guess; 
+			int guess = getAndCheckInput(); 
+			 
 			bool guessedCorrectly = checkGuess(number, guess);
 			if (guessedCorrectly) {
 				guessing = false;
@@ -81,10 +147,9 @@ int main()
 		if (correct) {
 			std::cout << guessCount << " guesses\n";
 		}
-		std::cout << "Would you like to play again? (y/n)\n";
-		char playAgain;
-		std::cin >> playAgain;
-		playAgain == 'y' ? (exitGame = false) : (exitGame = true);
+		
+		bool playAgain = checkPlayAgain();
+		playAgain ? (exitGame = false) : (exitGame = true); //sweet ternary operator 
 		
 		
 	}
@@ -93,4 +158,3 @@ int main()
 
     return 0;
 }
-
