a8218616c9cc29e81af9d468b627f772eb38980b
dillonallen92 <dillona@cos.edu>
2018-07-20 22:51:07 -0700

    made mouseClick event to spawn floating orbs

--- javascript_code/firstProgram/sketch.js
+++ javascript_code/firstProgram/sketch.js
@@ -1,9 +1,48 @@
+/*
+This is a floating ball program that creates floating balls on mouse click
+*/
+
+let balls = [];
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	let x = width/2;
-	let y = height/2;
-	rect(100, 200, x, y);
+	background(0);
+
+	for (let b of balls)
+	{
+		b.move();
+		b.display();
+	}
+}
+
+function mousePressed()
+{
+	var newBall = new floatingBall(mouseX, mouseY);
+	balls.push(newBall);
+}
+
+class floatingBall
+{
+	constructor(varX, varY, radius = 15, speed = 5)
+	{
+		this.x = varX;
+		this.y = varY;
+		this.r = radius;
+		this.speed = speed;
+	}
+
+	move()
+	{
+		this.x = this.x + random(-this.speed, this.speed);
+		this.y = this.y + random(-this.speed, this.speed);
+	}
+
+	display()
+	{
+		fill(255);
+		ellipse(this.x, this.y, this.r);
+	}
 }
