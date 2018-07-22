001d5f33173f4839a2514ed6202bc614a9ecde49
dillonallen92 <dillona@cos.edu>
2018-07-21 23:10:31 -0700

    started a collection of physics formula

---
+++ cpp_code/dillons_code/project1/Physics2d.cpp
@@ -0,0 +1,31 @@
+#include "Vector2d.h"
+#include "Physics2d.h"
+#include <cmath>
+#include <cfloat>
+
+// This uses mass and multiplies it by gravity going down on the y
+Vector2d weightVector(const float&m)
+{
+    Vector2d g = Vector2d(0,-9.81);
+    return g*m;
+}
+
+// This computes the acceleration vector from a force vector
+Vector2d accelerationVector(const float&m, const Vector2d&F)
+{
+    return (F / m);
+}
+
+// This takes the weight on a flat surface and gives it an incline, for basic incline problems
+Vector2d angledWeight(const Vector2d&W, const float&a)
+{
+    return Vector2d(W.x*cos(a), W.y*sin(a));
+
+}
+
+// This calculates work done over a distance d
+float WorkDone(Vector2d&F, const Vector2d&d)
+{
+    return (Vector2d::Dot(F,d));
+}
+
---
+++ cpp_code/dillons_code/project1/Physics2d.h
@@ -0,0 +1,15 @@
+#ifndef PHYSICS2D_H
+#define PHYSICS2D_H
+
+#include "Vector2d.h"
+
+Vector2d weightVector(const float&m, const Vector2d&a);
+
+Vector2d accelerationVector(const float&m, const Vector2d&F);
+
+Vector2d angledWeight(const Vector2d&W, const float&a);
+
+float WorkDone(const Vector2d&F, const Vector2d&d);
+
+
+#endif
