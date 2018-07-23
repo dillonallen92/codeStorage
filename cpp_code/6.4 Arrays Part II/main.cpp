076bd2ba0ba309dc935eec7b8da86fd5a57fc90d
brenden748 <34994038+brenden748@users.noreply.github.com>
2018-07-22 17:29:13 -0700

    Add files via upload

---
+++ cpp_code/6.4 Arrays Part II/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+
+    int array[7] = {3, 2, 4, 7, 1, 9, 0};
+
+    int arrayLength = sizeof(array) / sizeof(array[0]);
+
+
+    //lets organize the array from lowest to highest
+    //first find the smallest element
+    int smallestIndex = 0; //to get things started
+    int smallest = array[0];
+
+    for (int j = 0; j < arrayLength; j++)
+    {
+        for (int i = j; i < arrayLength; i++)
+        {
+            if (array[smallestIndex] > array[i])
+            {
+                smallestIndex = array[i];
+            }
+        }
+        std::swap(array[smallestIndex], array[j]);
+    }
+
+    //print new array
+    for (int i = 0; i <arrayLength; i++){
+        std::cout << array[i];
+    }
+}
+
