a65df4eec422bc6fd9bb788674dc035218b16d19
brenden748 <34994038+brenden748@users.noreply.github.com>
2018-07-22 19:49:02 -0700

    Add files via upload

--- cpp_code/Brenden's Code/Simple Array Sorting Algorithm/main.cpp
+++ cpp_code/Brenden's Code/Simple Array Sorting Algorithm/main.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for std::time
+#include <cmath>
+
+int getRandomNumber(int min,int max, int previousSeed) {
+	//generate using srand() and clock as seed
+	std::srand(static_cast<unsigned int>(std::time(0)) + previousSeed);
+	std::rand();
+
+	//create a fraction that will always put the number returned by rand() between 0-1
+	static const double fraction = (1.0 / (RAND_MAX + 1.0));  //note, if the 1.0's are not exactly "1.0" it messes up
+	//get the range that we are guessing int
+	int range = abs(max - min) + 1;
+
+	return (min + static_cast<int>( (fraction * range * std::rand())));
+}
 
 int main()
 {
-    int array[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    //int array[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    // lets make a random array
+    int previousSeed = 99;
+    int arrayMinLength = 10,arrayMaxLength = 80, arrayMinValue = 1, arrayMaxValue = 700;
+    int randomNum = getRandomNumber(arrayMinLength, arrayMaxLength, previousSeed); // this will be the length of the array
+    
 
+
+
+    int array[randomNum] = {0};
+    int lastRandom = 0; //purely for help debugging. Cant easily see inside arrays
+    for (int i = 0; i < randomNum; i++)
+    { // fills each index of the array with a random no.
+        array[i] = getRandomNumber(arrayMinValue, arrayMaxValue, previousSeed);
+        lastRandom = array[i];
+        previousSeed = lastRandom;
+        
+    }
     int arrayLength = sizeof(array) / sizeof(array[0]);
 
-    //lets organize the array from lowest to highest
+    // print this array just to see what it looks like
+    std::cout << "Input Array : \n";
+    for (int i = 0; i < arrayLength; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << "\n\n";
+
     
+
+    //lets organize the array from lowest to highest
+
     int smallestIndex = 0; //to get things started
     int smallest = array[0];
 
@@ -31,8 +73,9 @@ int main()
     }
 
     //print new array
+    std::cout<< "Output Array : \n";
     for (int i = 0; i < arrayLength; i++)
     {
-        std::cout << array[i];
+        std::cout << array[i] << " ";
     }
 }
