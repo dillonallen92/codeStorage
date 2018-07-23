02f4f9ce25518d4c73a694f55263bb7e6dc3fd4c
brenden748 <34994038+brenden748@users.noreply.github.com>
2018-07-22 17:33:03 -0700

    Update main.cpp

--- cpp_code/6.4 Arrays Part II/main.cpp
+++ cpp_code/6.4 Arrays Part II/main.cpp
@@ -9,12 +9,12 @@ int main()
 
 
     //lets organize the array from lowest to highest
-    //first find the smallest element
+    
     int smallestIndex = 0; //to get things started
     int smallest = array[0];
-
+    //the outer loop does the swapping
     for (int j = 0; j < arrayLength; j++)
-    {
+    {   //this loop should return the smallest in the range from i=j to arraylength
         for (int i = j; i < arrayLength; i++)
         {
             if (array[smallestIndex] > array[i])
