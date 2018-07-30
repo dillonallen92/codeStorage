991089ab983c04b0f8f0ef8ecda7afa62d59efbb
dillonallen92 <dillona@cos.edu>
2018-07-30 10:41:28 -0700

    changed comment style

--- python_code/dillonsCode/algorithms/nodes.py
+++ python_code/dillonsCode/algorithms/nodes.py
@@ -1,13 +1,13 @@
-''' this is taken from a website on algorithms '''
+# this is taken from a website on algorithms
 
-''' example code here '''
+# example code here
 
 class daynames:
     def __init__(self, dataval = None):
         self.dataval = dataval
         self.nextval = None
 
-''' Create instances of days '''
+# Create instances of days
 
 e1 = daynames('Mon')
 e2 = daynames('Tues')
@@ -17,7 +17,7 @@ e5 = daynames('Fri')
 e6 = daynames('Sat')
 e7 = daynames('Sun')
 
-''' Create the pointers from one day to the next '''
+# Create the pointers from one day to the next
 
 e1.nextval = e2
 e2.nextval = e3
@@ -26,14 +26,14 @@ e4.nextval = e5
 e5.nextval = e6
 e6.nextval = e7
 
-''' Initialize the first value '''
+# Initialize the first value
 
 thisvalue = e1
 
-''' Transverse the nodes '''
+# Transverse the nodes
 
 while thisvalue:
     print(thisvalue.dataval)
     thisvalue = thisvalue.nextval
 
-''' example code end '''
+# example code end
