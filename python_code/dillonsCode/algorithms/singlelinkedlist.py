2676907dbc0a91e3dd96d78ad590a18e9db9ad22
dillonallen92 <dillona@cos.edu>
2018-07-30 19:24:11 -0700

    finished single linked list, with example

--- python_code/dillonsCode/algorithms/singlelinkedlist.py
+++ python_code/dillonsCode/algorithms/singlelinkedlist.py
@@ -47,8 +47,50 @@ class SinglyLinkedList:
         while(laste.nextval):
             laste = laste.nextval
         laste.nextval = NewNode
-        
+    
+    def insertInbetween(self, middle_node, newData):
 
+        # Check if the node exists
+
+        if middle_node is None:
+            print("The node you requested is misisng")
+            return
+        
+        # Now Place after the mentioned node
+
+        NewNode = nodes.Node(newData)
+        NewNode.nextval = middle_node.nextval
+        middle_node.nextval = NewNode
+        
+    def removeItem(self, RemoveKey):
+
+        # Setting the headval logic
+
+        HeadVal = self.headval
+        if HeadVal is not None:
+            if HeadVal.dataval == RemoveKey:
+                self.headval = HeadVal.nextval
+                HeadVal = None
+                return
+
+        # Traverse list to find the key and remove the object, placing the next value in its place
+
+        while HeadVal is not None:
+            if HeadVal.dataval == RemoveKey:
+                break
+            prev = HeadVal
+            HeadVal = HeadVal.nextval
+
+        # Handles null head value
+
+        if HeadVal == None:
+            return
+        
+        prev.nextval = HeadVal.nextval
+
+        HeadVal = None
+
+        
 # Create the list
 
 list1 = SinglyLinkedList()
@@ -84,5 +126,17 @@ list1.listprint()
 list1.insertAtEnd("Thurs")
 print("*"*20)
 print("This is now with ('Thurs') added to the end of the list")
+list1.listprint()
+print("*"*20)
+
+# Add ( "Its wednesday my dudes ahhhh" ) after Wednesday and before Thursday
+# Note that the nodes shifted so e2.nextval is now e3 from the original link
+
+list1.insertInbetween(e2.nextval, "It's Wednesday my dudes, AHHHHHHH")
+list1.listprint()
+print("*"*20)
+
+# Now I will remove the recent addition
+list1.removeItem("It's Wednesday my dudes, AHHHHHHH")
 list1.listprint()
 print("*"*20)
