48f2201fad00568d29b1589529b29297be16f8bb
dillonallen92 <dillona@cos.edu>
2018-12-12 19:50:16 -0800

    error handling added for poorly typed filenames

--- cpp_code/dillons_code/project3/mlCLI.py
+++ cpp_code/dillons_code/project3/mlCLI.py
@@ -23,60 +23,83 @@ def FileManager():
     fileName = ""
     if (fileName == ""):
         fileName = ""
-        print("File Manager Menu")
+        print("\n --- File Manager Menu --- \n")
         print("1. Display everything in pwd")
         print("2. Input filename")
         print("Enter mm for main menu")
-        userInput = input("What would you like to do? ")
+        userInput = input("\nWhat would you like to do? ")
         if ( userInput == "mm"):
             main()
         if ( userInput == "1"):
             print(os.listdir())
             FileManager()
         if ( userInput == "2"):
-            finput = input("Is the file a .txt or .csv? ( txt / csv ): ")
+            finput = input("\nIs the file a .txt or .csv? ( txt / csv ): ")
             if (finput == 'txt'):
-                fname = input("Enter the filename ( without the .txt): ")
+                fname = input("\nEnter the filename ( without the .txt): ")
                 fileName = fileName + fname + ".txt"
                 print(fileName)
-                yninput = input("Does this look right to you? ( y / n ):  ")
+                yninput = input("\nDoes this look right to you? ( y / n ):  ")
                 if (yninput == 'y'):
                     EDA(fileName)
                 if (yninput == 'n'):
                     FileManager()
             if (finput == "csv"):
-                fname = input("Enter the filename ( without the .csv): ")
+                fname = input("\nEnter the filename ( without the .csv): ")
                 fileName = fileName + fname + ".csv"
                 print(fileName)
-                yninput = input("Does this look right to you? ( y / n ):  ")
+                yninput = input("\nDoes this look right to you? ( y / n ):  ")
                 if (yninput == 'y'):
                     EDA(fileName)
                 if (yninput == 'n'):
                     FileManager()
             
-def EDA(fileName):
+def EDA(fileName=""):
+    if fileName == "":
+        FileManager()
+    print("\n --- EDA Section --- \n")
     print(fileName)
-    yninput = input("Is this the correct fileName? ( y / n ): ")
+    yninput = input("\nIs this the correct fileName? ( y / n ): ")
     if (yninput == "y"):
-        df = pd.read_csv(fileName)
-        done = False
-        while ( done == False ):
-            print("1. Display head of dataframe")
-            print("2. Display dataframe sumamry")
-            print("3. Display the scatter matrix")
-            uInput = input("What would you like to see? ")
-            if (uInput == '1'):
-                print(df.head())
-            if (uInput == '2'):
-                print(df.describe())
-            if (uInput == '3'):
-                scatter_matrix(df, alpha = 0.2)
-                plt.show()
-            doneYet = input("Are you done yet? ( y / n ): ")
-            if (doneYet == 'y'):
-                done = True
-            else:
-                pass
+        try:
+            df = pd.read_csv(fileName)
+            done = False
+            while ( done == False ):
+                print("\n1. Display head of dataframe")
+                print("2. Display dataframe sumamry")
+                print("3. Display the scatter matrix")
+                print("4. Display Features List: ")
+                uInput = input("\nWhat would you like to see? ")
+                if (uInput == '1'):
+                    print("")
+                    print(df.head())
+                if (uInput == '2'):
+                    print("")
+                    print(df.describe())
+                if (uInput == '3'):
+                    scatter_matrix(df, alpha = 0.2)
+                    plt.show()
+                if (uInput == "4"):
+                    print("")
+                    print(list(df.columns.values))
+                    pltYN = input("\nWould you like to plot two categorical values against each other? ( y / n ): ")
+                    if pltYN == 'y':
+                        val1 = input("\nEnter categorical value 1: ")
+                        val2 = input("\nEnter categorical value 2: ")
+                        plt.scatter(df[:][val1], df[:][val2])
+                        plt.xlabel(val1)
+                        plt.ylabel(val2)
+                        plt.show()
+                doneYet = input("\nAre you done yet? ( y / n ): ")
+                if (doneYet == 'y'):
+                    done = True
+                    main()
+                else:
+                    pass
+        except:
+            print("\n --- Error in file naming (Not found or misspelled) --- \n")
+            print("\n --- Sending back to File Manager Menu --- \n")
+            FileManager()
     if (yninput == 'n'):
         FileManager()
     
@@ -88,21 +111,22 @@ def optionThree():
     print("option Three")
 
 def main():
-    print("Machine Learning Command Line Interface")
+    print("\n --- Machine Learning Command Line Interface --- \n")
     print("1. File Manager ")
-    print("2. Option 2")
+    print("2. EDA ")
     print("3. Option 3")
     print("4. Quit")
 
-    userInput = input("What would you like to do? ")
+    userInput = input("\nWhat would you like to do? ")
 
     if ( userInput == "1" ):
         FileManager()
     if ( userInput == "2" ):
-        optionTwo()
+        EDA()
     if ( userInput == "3" ):
         optionThree()
     if ( userInput == "4" ):
+        print("\n --- Exiting Program --- \n")
         SystemExit
 
 # Main statement here to start the program
