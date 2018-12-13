4f0dc90ede9df19e641cab0a249eeda8aacc9b88
dillonallen92 <dillona@cos.edu>
2018-12-12 16:37:22 -0800

    Created menu, file manager, and started exploratory data analysis

--- cpp_code/dillons_code/project3/Outline.markdown
+++ cpp_code/dillons_code/project3/Outline.markdown
@@ -1,3 +1,18 @@
 # Project Idea
 
 ## Create a CLI where you run a .py file on a csv or txt, user will be prompted to choose a set of analyses.
+
+## main() : displays the menu of options and allows the user to choose
+1. EDA() - Exploratory Data Analysis
+    * Display file heirarchy 
+    * Have user input which txt or csv file to analyze
+    * Option to display all features
+    * scatter_matrix option
+    * Allow user to choose which features to plot
+    * go to main menu or MLKit()
+2. MLKit() - List of ML methods, test-train-split, cross validation
+    * Regressions
+    * train-test-split and value tuning
+    * models and value adjustments with confusion matrix results
+    * k-fold cross validation, user specifies k
+    * go to main menu or EDA
--- cpp_code/dillons_code/project3/mlCLI.py
+++ cpp_code/dillons_code/project3/mlCLI.py
@@ -1,10 +1,80 @@
+'''
+    Command Line Interface for Machine Learning 
+    Written by Dillon Allen 
+    December 12th, 2018
+
+    Using test file 
+'''
 
 # Imports
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
 # Globals
 
-def optionOne():
-    print("option One")
+fileName = ""
+userInput = ""
+
+def FileManager():
+    fileName = ""
+    if (fileName == ""):
+        fileName = ""
+        print("File Manager Menu")
+        print("1. Display everything in pwd")
+        print("2. Input filename")
+        print("Enter mm for main menu")
+        userInput = input("What would you like to do? ")
+        if ( userInput == "mm"):
+            main()
+        if ( userInput == "1"):
+            print(os.listdir())
+            FileManager()
+        if ( userInput == "2"):
+            finput = input("Is the file a .txt or .csv? ( txt / csv ): ")
+            if (finput == 'txt'):
+                fname = input("Enter the filename ( without the .txt): ")
+                fileName = fileName + fname + ".txt"
+                print(fileName)
+                yninput = input("Does this look right to you? ( y / n ):  ")
+                if (yninput == 'y'):
+                    EDA(fileName)
+                if (yninput == 'n'):
+                    FileManager()
+            if (finput == "csv"):
+                fname = input("Enter the filename ( without the .csv): ")
+                fileName = fileName + fname + ".csv"
+                print(fileName)
+                yninput = input("Does this look right to you? ( y / n ):  ")
+                if (yninput == 'y'):
+                    EDA(fileName)
+                if (yninput == 'n'):
+                    FileManager()
+            
+def EDA(fileName):
+    print(fileName)
+    yninput = input("Is this the correct fileName? ( y / n ): ")
+    if (yninput == "y"):
+        df = pd.read_csv(fileName)
+        done = False
+        while ( done == False ):
+            print("1. Display head of dataframe")
+            print("2. Display dataframe sumamry")
+            uInput = input("What would you like to see? ")
+            if (uInput == '1'):
+                print(df.head())
+            if (uInput == '2'):
+                print(df.describe())
+            doneYet = input("Are you done yet? ( y / n ): ")
+            if (doneYet == 'y'):
+                done = True
+            else:
+                pass
+    if (yninput == 'n'):
+        FileManager()
+    
 
 def optionTwo():
     print("option Two")
@@ -14,18 +84,21 @@ def optionThree():
 
 def main():
     print("Machine Learning Command Line Interface")
-    print("1. Option 1")
+    print("1. File Manager ")
     print("2. Option 2")
     print("3. Option 3")
+    print("4. Quit")
 
     userInput = input("What would you like to do? ")
 
-    if ( userInput == "1" ) :
-        optionOne()
-    if ( userInput == "2" ) :
+    if ( userInput == "1" ):
+        FileManager()
+    if ( userInput == "2" ):
         optionTwo()
     if ( userInput == "3" ):
         optionThree()
+    if ( userInput == "4" ):
+        SystemExit
 
 # Main statement here to start the program
 
