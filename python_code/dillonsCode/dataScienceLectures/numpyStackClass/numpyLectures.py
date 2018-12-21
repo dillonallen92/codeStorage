7116fe72db884019a33fb42b94331912dae63649
dillonallen92 <dillona@cos.edu>
2018-12-20 18:30:57 -0800

    datascience master material

---
+++ python_code/dillonsCode/dataScienceLectures/numpyStackClass/numpyLectures.py
@@ -0,0 +1,21 @@
+'''
+    Lecture Notes for Numpy Stack class
+'''
+
+import numpy as np
+
+L = [1,2,3]
+
+A = np.array([1,2,3])
+
+for e in L:
+    print(e)
+
+for e in A:
+    print(e)
+
+print(2*A)
+print(A**2)
+
+# We can treat np arrays as vectors
+
