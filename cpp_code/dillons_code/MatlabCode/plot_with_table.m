8a128e7008dd238a330b6d8409cb468c7bc56a1d
dillonallen92 <dillona@cos.edu>
2019-01-16 07:31:41 -0800

    Matlab Tutorial Code
    Matlab code

---
+++ cpp_code/dillons_code/MatlabCode/plot_with_table.m
@@ -0,0 +1,12 @@
+% Plotting Code and Display Table
+
+v0 = input("Enter your initial speed (m/s): ");
+theta = input('Enter your elevation angle: ');
+theta = theta * pi / 180;
+g = 9.81; t = 0:0.1:1;
+x = v0*cos(theta)*t;
+y = v0*sin(theta)*t - .5*g*t.^2;
+Table = [t', x', y'];
+disp('    ')
+disp('    Time (s)    x(m)     y(m)' )
+disp( Table )
