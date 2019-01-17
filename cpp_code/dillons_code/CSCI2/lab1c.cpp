41b05f91e49e2db59180432113d2e4552ac0630f
dillonallen92 <dillona@cos.edu>
2019-01-17 13:21:55 -0800

    fixed errors in 1c

--- cpp_code/dillons_code/CSCI2/lab1c.cpp
+++ cpp_code/dillons_code/CSCI2/lab1c.cpp
@@ -21,21 +21,18 @@ int main()
     std::cout << "Compare two ints (23 vs -65). Min: " << getMin(23, -65) << ". Max: " << getMax(23, -65) << '\n';
     std::cout << "Compare two doubles (2.4, -2.7). Min: " << getMin(2.4, -2.7) << ". Max: " << getMax(2.4, -2.7) << '\n';
     std::cout << "Compare two chars (c, f). Min: " << getMin('c', 'f') << ". Max: " << getMax('c', 'f') << '\n';
-    std::cout << "Compare two strings (zebra, beach). Min: " << getMin(static_cast<std::string>("zebra"), static_cast<std::string>("beach")) << ". Max: " << getMax("zebra", "beach") << '\n';
+    std::cout << "Compare two strings (zebra, beach). Min: " << getMin(static_cast<std::string>("zebra"), static_cast<std::string>("beach")) << ". Max: " << getMax(static_cast<std::string>("zebra"), static_cast<std::string>( "beach")) << '\n';
     return 0;
 }
 
 template <class T>
 T getMin(T a, T b)
 {
-    if( a > b )
-        return b;
-    return(a);
+    return((a>b) ? b : a);
 }
 
 template <class T>
 T getMax(T a, T b)
-{   if ( a > b )
-        return a;
-    return(b);
+{   
+    return((a > b) ? a : b);
 }
