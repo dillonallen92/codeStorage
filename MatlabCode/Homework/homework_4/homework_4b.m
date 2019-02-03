16cbaf505420b6c3122f90a0d03ba7f6ca36e3a2
dillonallen92 <dillona@cos.edu>
2019-02-02 18:06:13 -0800

    updated spacing

--- MatlabCode/Homework/homework_4/homework_4b.m
+++ MatlabCode/Homework/homework_4/homework_4b.m
@@ -1,16 +1,6 @@
-% Create a script to print the table of values for a Lemniscate of Bernoulli
-% Create a subfunction to handle creating the x and y values
-% Include a local function to print the table of values with theta, x, y
-% Plot the curve
-
-clear, clc, close all
-
-printTableOfValues;
-
-function printTableOfValues
+function homework_4b(a)
     global a;
     global theta;
-    a = 5;
     theta = -2*pi : pi / 64 : 2*pi;
     disp('This is a function that prints out the value of a table');
 
@@ -29,4 +19,3 @@ function printTableOfValues
     end
 
 end
-
