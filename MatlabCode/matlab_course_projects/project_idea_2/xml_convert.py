cb7e385867430624846a6a9dcb3ece18c99c66ec
dillonallen92 <dillona@cos.edu>
2019-03-03 16:23:17 -0800

    xml_converter for matlab

---
+++ MatlabCode/matlab_course_projects/project_idea_2/xml_convert.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+import sys
+import collections
+
+def convertXMLtoDict(file):
+
+
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        dateArray = []
+        valueArray = []
+
+        for i in range(len(root)):
+                if (root[i].get('type') == 'HKQuantityTypeIdentifierHeartRate'):
+                        dateArray.append(root[i].attrib['startDate'])
+                        valueArray.append(float(root[i].attrib['value']))
+
+        heartRateDict = collections.OrderedDict(zip(dateArray, valueArray))
+        return heartRateDict
+
+if __name__ == '__main__':
+        xmlFileName = sys.argv[1]
+        myDict = convertXMLtoDict(xmlFileName)
+        sys.stdout.write(myDict)
