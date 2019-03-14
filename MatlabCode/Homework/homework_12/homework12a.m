049398a7bf69278dfbba4d50d323ea69b1cd9332
dillonallen92 <dillona@cos.edu>
2019-03-14 13:29:01 -0700

    homework12a before while loops

---
+++ MatlabCode/Homework/homework_12/homework12a.m
@@ -0,0 +1,19 @@
+% Rewrite example03_04b using while-loops instead of for-loops
+
+clear;
+x = linspace(0,1,30);
+y = linspace(0,1,40);
+[X Y] = meshgrid(x,y);
+Phi = zeros(40,30);
+for i = 1:40
+    for j=1:30
+        for k=1:20
+            Phi(i,j) = Phi(i,j) + 4*(1-cos(k*pi))/(k*pi)^3 ...
+            *exp(-k*X(i,j)*pi)*sin(k*Y(i,j)*pi);
+        end
+    end
+end
+surf(x,y,Phi)
+xlabel('\itx')
+ylabel('\ity')
+zlabel('\phi(\itx\rm, \ity\rm)')
