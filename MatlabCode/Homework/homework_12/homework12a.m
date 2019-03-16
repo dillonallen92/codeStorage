ff892e606be77d61844dd14f3be86f348f8814b1
dillonallen92 <dillona@cos.edu>
2019-03-16 12:24:42 -0700

    homework12a complete

--- MatlabCode/Homework/homework_12/homework12a.m
+++ MatlabCode/Homework/homework_12/homework12a.m
@@ -3,16 +3,29 @@
 clear;
 x = linspace(0,1,30);
 y = linspace(0,1,40);
-[X Y] = meshgrid(x,y);
+[X , Y] = meshgrid(x,y);
 Phi = zeros(40,30);
-for i = 1:40
-    for j=1:30
-        for k=1:20
-            Phi(i,j) = Phi(i,j) + 4*(1-cos(k*pi))/(k*pi)^3 ...
-            *exp(-k*X(i,j)*pi)*sin(k*Y(i,j)*pi);
-        end
-    end
-end
+
+% For loop implementation
+
+% for i = 1:40
+%     for j=1:30
+%         for k=1:20
+%             Phi(i,j) = Phi(i,j) + 4*(1-cos(k*pi))/(k*pi)^3 ...
+%             *exp(-k*X(i,j)*pi)*sin(k*Y(i,j)*pi);
+%         end
+%     end
+% end
+
+% While loop implementation
+
+k = 1;
+ while k <= 20
+    Phi= Phi + 4*(1-cos(k*pi))/(k*pi)^3 ...
+    *exp(-k*X.*pi).*sin(k*Y*pi);
+     k = k + 1;
+ end
+
 surf(x,y,Phi)
 xlabel('\itx')
 ylabel('\ity')
