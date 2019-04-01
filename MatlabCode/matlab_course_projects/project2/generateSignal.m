26b68f0d3df1a72c9ec7bd42c32187a21fa55bd5
dillonallen92 <dillona@cos.edu>
2019-03-31 18:49:02 -0700

    probably scrapping

--- MatlabCode/matlab_course_projects/project2/generateSignal.m
+++ MatlabCode/matlab_course_projects/project2/generateSignal.m
@@ -2,13 +2,18 @@
 % Dillon Allen
 % Goal: Design a class that allows the user to create a signal and implement 
 % a low-pass, high-pass and bandpass filter. Plot all results.
+% Probably scrapping this project for another one
 
 classdef generateSignal
+    % This is a class that generates signals and has a collection of filters.
+    % Arguments are (amplitude, sample_freq, time_Stop, lower_bound for freq, upper_bound for freq, rand_noise_bool)
     properties
         a = 0;
         fs = 0;
         ts = 0;
         t = 0;
+        lowf = 0;
+        highf = 0;
         randFuzzBool = false;
         randFuzz = 0;
         inputSignal = 0;
@@ -20,12 +25,15 @@ classdef generateSignal
             mySignal.a = varargin{1}; 
             mySignal.fs = varargin{2};
             mySignal.ts = varargin{3};
-            mySignal.randFuzzBool = varargin{4};
+            mySignal.lowf = varargin{4};
+            mySignal.highf = varargin{5};
+            mySignal.randFuzzBool = varargin{6};
             mySignal.t = 0 : 1/(mySignal.fs) : mySignal.ts;
+            freq_range = linspace(mySignal.lowf, mySignal.highf, 1/(mySignal.fs));
             if mySignal.randFuzzBool
                 mySignal.randFuzz = randn(size(mySignal.t))/10;
             end
-            mySignal.inputSignal = mySignal.a*sin(2*pi.*mySignal.t) + mySignal.randFuzz; 
+            mySignal.inputSignal = mySignal.a*sin(2*pi.*freq_range.*mySignal.t) + mySignal.randFuzz; 
         end
 
         function disp(mySignal)
