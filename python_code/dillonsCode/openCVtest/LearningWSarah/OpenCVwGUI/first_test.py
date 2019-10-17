d61be02de2243a15165cbc46d99e82310ee17134
Dillon Allen <dillona@cos.edu>
2019-10-16 18:01:37 -0700

    first qt tutorial done, dont like this package

--- python_code/dillonsCode/openCVtest/LearningWSarah/OpenCVwGUI/first_test.py
+++ python_code/dillonsCode/openCVtest/LearningWSarah/OpenCVwGUI/first_test.py
@@ -1,1 +1,8 @@
-print("hello world!")
+# This test will make a simple Qt Window
+# Then I will build on top of this
+
+from PyQt5.QtWidgets import QApplication, QLabel
+app = QApplication([])
+label = QLabel("Hello World!")
+label.show()
+app.exec_()
