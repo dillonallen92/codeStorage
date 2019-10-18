00589fda2bab4e656f0eec2e3e9cc1415b9397d8
Dillon Allen <dillona@cos.edu>
2019-10-18 16:47:00 -0700

    cleaned up code

--- python_code/dillonsCode/openCVtest/LearningWSarah/OpenCVwGUI/rockpaperscissors.py
+++ python_code/dillonsCode/openCVtest/LearningWSarah/OpenCVwGUI/rockpaperscissors.py
@@ -4,29 +4,31 @@
 import random
 
 # Infinite loop to keep the game state going
+print("Rock paper scissors game!")
 while True:
-    print("Rock paper scissors game!")
 
     # User Choice here
     user_choice = int(input("Enter your choice below: \n 1. Rock \n 2. Paper \n 3. Scissors \n User Input: "))
     if user_choice > 3 or user_choice < 1:
         user_choice = int(input("Enter your choice below: \n 1. Rock \n 2. Paper \n 3. Scissors \n User Input: "))
     if user_choice == 1:
-        print("You chose Rock!")
+        choice_name = "Rock"
     elif user_choice == 2:
-        print("You chose Paper!")
+        choice_name = "Paper"
     else:
-        print("You chose Scissors!")
+        choice_name = "Scissors"
+    print("You chose:", choice_name)
 
     # Computer Choice
     comp_choice = int(random.randint(1,3))
     if comp_choice == 1:
-        print("Computer chose Rock!")
+        computer_choice_name = "Rock"
     elif comp_choice == 2:
-        print("Computer chose Paper!")
+        computer_choice_name = "Paper"
     else:
-        print("Computer chose Scissors!")
-    
+        computer_choice_name = "Scissors"
+    print ("Computer chose:", computer_choice_name)
+
     # Evaluation Loop
     if user_choice == 1 and comp_choice == 3:
         print("Player wins.")
