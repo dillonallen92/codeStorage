89803883cc1f9dafe1edaa1fac10b7a7e0e15f15
Dillon Allen <dillona@cos.edu>
2021-01-13 12:08:01 -0800

    intro to competitive programming - getting basic programs to work in cpp again

---
+++ cpp_code/dillons_code/Competitive Programming Stuff/Competitive Programming Book Notes/first_section/first_chp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // Messing with inputs and outputs in C++ again
+    string s;
+    cout << "Enter your name: "; 
+    cin >> s;
+    cout << "Your name is " << s << "\n";
+}
+
